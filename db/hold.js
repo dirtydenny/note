b603b5f908a14b85b6bfa58961d2997d84b9d390
dirtydenny <dallen7@socal.rr.com>
2023-03-24 14:35:10 -0700

    write read

--- db/hold.js
+++ db/hold.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 
 
 const { v4: uuidv4 } = require("uuid");
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
