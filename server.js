ce50ef44a12bc81d5dd0c662d3ce59c57a0c7a6d
dirtydenny <dallen7@socal.rr.com>
2023-03-24 12:56:28 -0700

    changes

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
+const express = require('express');
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
-const express = require("express");
-const path = require("path");
-
-
 const app = express();
 const PORT = process.env.port || 3001;
-
+// parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 app.use(express.static("public"));
-
+//set server to listen
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
